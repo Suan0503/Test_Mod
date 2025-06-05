d6b63b318ea547bcaa48210be0c42d5e7b00d043
Suan0503 <a12365478957@gmail.com>
2025-06-05 23:29:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ load_dotenv()
 
 from extensions import db
 from routes.message import message_bp
-from models import CouponModel  # 確保 models 初始化
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
@@ -20,7 +19,7 @@ def home():
     return "LINE Bot 正常運作中～🍵"
 
 if __name__ == "__main__":
-    # 初次部署時可用這行建立資料表
+    # 首次部署時可以加上以下建立資料表
     # with app.app_context():
     #     db.create_all()
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
