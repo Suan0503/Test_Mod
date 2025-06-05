c3718396605fb9b67926cf91793e71401177b736
Suan0503 <a12365478957@gmail.com>
2025-06-05 22:46:29 +0800

    Update message.py

--- routes/message.py
+++ routes/message.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, request, abort
+# 這裡可根據你的架構加上 line_bot_api, handler
 
 message_bp = Blueprint('message', __name__)
 
 @message_bp.route("/callback", methods=["POST"])
 def callback():
+    # 處理 LINE webhook 或其它邏輯
     return "OK"
