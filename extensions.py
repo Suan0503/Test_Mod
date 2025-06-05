afc567cc38c3b105b03d31aaf7a1f0027e68333e
Suan0503 <a12365478957@gmail.com>
2025-06-05 22:57:36 +0800

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -1,8 +1,16 @@
+from linebot import LineBotApi, WebhookHandler
 from flask_sqlalchemy import SQLAlchemy
 import redis
 import os
 
+# LINE Bot
+line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
+
+# SQLAlchemy
 db = SQLAlchemy()
+
+# Redis
 redis_client = redis.Redis(
     host=os.getenv("REDIS_HOST", "localhost"),
     port=int(os.getenv("REDIS_PORT", 6379)),
