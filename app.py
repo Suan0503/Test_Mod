5d0460124290a826df60ae5a7d6bc7f934913108
Suan0503 <a12365478957@gmail.com>
2025-06-06 01:46:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from extensions import db
 from routes.message import message_bp
 
 app = Flask(__name__)
-app.config.from_pyfile('config.py', silent=True)  # 或用 os.environ/Getenv 等
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
 app.register_blueprint(message_bp)
 
+@app.route("/")
+def home():
+    return "LINE Bot 正常運作中～🍵"
+
 if __name__ == "__main__":
-    app.run()
+    # 首次佈署可取消註解以下兩行建立資料表
+    # with app.app_context():
+    #     db.create_all()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
