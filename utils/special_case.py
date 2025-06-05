10be399ceb06e4c7bfdc396fc7658f6b7f632063
Suan0503 <a12365478957@gmail.com>
2025-06-06 01:57:55 +0800

    Update special_case.py

--- utils/special_case.py
+++ utils/special_case.py
@@ -1,6 +1,4 @@
 def is_special_case(user_id):
-    # 這裡回傳 True/False，依你的專案補上特殊 case 判斷
-    special_ids = [
-        # 範例: "Uxxxxxx"
-    ]
+    # 依你的規則判斷
+    special_ids = set()
     return user_id in special_ids
