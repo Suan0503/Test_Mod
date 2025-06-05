e24b329b244362e3d8c6aae0b2a0f56e3179eb3a
Suan0503 <a12365478957@gmail.com>
2025-06-05 23:00:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from extensions import db, line_bot_api, handler, redis_client
 from routes.message import message_bp
+import os
+from dotenv import load_dotenv
+
+# 載入 .env 設定（建議加這一行，讓本地/部署都能自動抓 .env）
+load_dotenv()
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
+# 註冊 Blueprint
 app.register_blueprint(message_bp)
 
 @app.route("/")
