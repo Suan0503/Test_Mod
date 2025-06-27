50593cab946590ed218bc639b703eec785e9de0d
Suan0503 <a12365478957@gmail.com>
2025-06-27 21:54:23 +0800

    Create follow.py

---
+++ hander/follow.py
@@ -0,0 +1,11 @@
+from linebot.models import FollowEvent, TextSendMessage
+from extensions import handler, line_bot_api
+
+@handler.add(FollowEvent)
+def handle_follow(event):
+    msg = (
+        "歡迎加入🍵茗殿🍵\n"
+        "請正確按照步驟提供資料配合快速驗證\n\n"
+        "➡️ 請輸入手機號碼進行驗證（含09開頭）"
+    )
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
