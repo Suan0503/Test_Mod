b59461ba9d6f6b6d7ef4f46408db37555b663ebc
Suan0503 <a12365478957@gmail.com>
2025-06-28 00:29:56 +0800

    Update models.py

--- models.py
+++ models.py
@@ -28,3 +28,4 @@ class Coupon(db.Model):
     date = db.Column(db.String(20))
     amount = db.Column(db.Integer)
     created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    
