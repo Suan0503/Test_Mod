5b7bd6551083d4c7c7eb401fc5f09019a141ea88
Suan0503 <a12365478957@gmail.com>
2025-06-27 23:13:28 +0800

    Update whitelist.py

--- models/whitelist.py
+++ models/whitelist.py
@@ -1,13 +1,12 @@
 from extensions import db
-from datetime import datetime
 
 class Whitelist(db.Model):
-    __tablename__ = "whitelist"
+    __tablename__ = 'whitelist'
     id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-    date = db.Column(db.String(20))
-    phone = db.Column(db.String(20), unique=True)
-    reason = db.Column(db.Text)
-    name = db.Column(db.String(255))
-    line_id = db.Column(db.String(100))
-    line_user_id = db.Column(db.String(255), unique=True)
+    phone = db.Column(db.String(20), unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=True)
+    line_id = db.Column(db.String(100), nullable=True)
+    line_user_id = db.Column(db.String(100), unique=True, nullable=True)
+    reason = db.Column(db.String(255), nullable=True)
+    date = db.Column(db.String(20), nullable=True)
+    created_at = db.Column(db.DateTime, nullable=False)
