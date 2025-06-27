1d46e0119febd6c0ebfbd8829424849de9034242
Suan0503 <a12365478957@gmail.com>
2025-06-27 21:39:43 +0800

    Update blacklist.py

--- models/blacklist.py
+++ models/blacklist.py
@@ -4,8 +4,8 @@ from datetime import datetime
 class Blacklist(db.Model):
     __tablename__ = "blacklist"
     id = db.Column(db.Integer, primary_key=True)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-    date = db.Column(db.String(20))
     phone = db.Column(db.String(20), unique=True)
-    reason = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     name = db.Column(db.String(255))
+    line_id = db.Column(db.String(100))
+    line_user_id = db.Column(db.String(255), unique=True)
