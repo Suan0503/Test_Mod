7ab0982dbb3c8c95cf4fabf8bafb4b4e64f12c8f
Suan0503 <a12365478957@gmail.com>
2025-06-27 21:39:52 +0800

    Update coupon.py

--- models/coupon.py
+++ models/coupon.py
@@ -4,7 +4,8 @@ from datetime import datetime
 class Coupon(db.Model):
     __tablename__ = "coupon"
     id = db.Column(db.Integer, primary_key=True)
-    line_user_id = db.Column(db.String(255))
-    date = db.Column(db.String(20))
-    amount = db.Column(db.Integer)
+    code = db.Column(db.String(50), unique=True)
+    is_used = db.Column(db.Boolean, default=False)
+    used_by = db.Column(db.String(255))
+    used_at = db.Column(db.DateTime)
     created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
