0ead897c4854c01ef0dec61181dc688eaa4dcedb
Suan0503 <a12365478957@gmail.com>
2025-06-27 23:13:38 +0800

    Update whitelist.py

--- models/whitelist.py
+++ models/whitelist.py
@@ -1,12 +1,8 @@
 from extensions import db
 
-class Whitelist(db.Model):
-    __tablename__ = 'whitelist'
+class Blacklist(db.Model):
+    __tablename__ = 'blacklist'
     id = db.Column(db.Integer, primary_key=True)
     phone = db.Column(db.String(20), unique=True, nullable=False)
-    name = db.Column(db.String(100), nullable=True)
-    line_id = db.Column(db.String(100), nullable=True)
-    line_user_id = db.Column(db.String(100), unique=True, nullable=True)
     reason = db.Column(db.String(255), nullable=True)
-    date = db.Column(db.String(20), nullable=True)
     created_at = db.Column(db.DateTime, nullable=False)
