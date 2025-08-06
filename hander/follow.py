38ad92b391e5b0d153ad20e7e7018dc999a2864b
Suan0503 <a12365478957@gmail.com>
2025-08-06 22:25:31 +0800

    Update follow.py

--- hander/follow.py
+++ hander/follow.py
@@ -5,12 +5,14 @@ from extensions import handler, line_bot_api
 def handle_follow(event):
     welcome_msg = (
         "歡迎加入🍵茗殿🍵\n"
+        "\n"
         "📜 驗證流程如下：\n"
         "1️⃣ 閱讀規則後點擊『我同意規則』\n"
         "2️⃣ 依步驟輸入手機號與 LINE ID\n"
         "3️⃣ 上傳 LINE 個人檔案截圖\n"
         "4️⃣ 系統進行快速自動驗證\n"
-        "5️⃣ 如無法辨識將交由客服人工處理\n\n"
+        "5️⃣ 如無法辨識將交由客服人工處理\n"
+        "\n"
         "✅ 完成驗證即可解鎖專屬客服＆預約功能💖"
     )
     line_bot_api.reply_message(
