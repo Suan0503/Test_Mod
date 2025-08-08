a5bc262fd529405a7a2978a7d36eb978bb514424
Suan0503 <a12365478957@gmail.com>
2025-08-08 17:51:21 +0800

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -3,5 +3,20 @@ from linebot import LineBotApi, WebhookHandler
 import os
 
 db = SQLAlchemy()
-line_bot_api = LineBotApi(os.environ["LINE_CHANNEL_ACCESS_TOKEN"])
+
+class DebugLineBotApi(LineBotApi):
+    def reply_message(self, *args, **kwargs):
+        import traceback
+        print("=== [DEBUG] reply_message 被呼叫 ===")
+        print("args:", args)
+        print("kwargs:", kwargs)
+        traceback.print_stack()
+        return super().reply_message(*args, **kwargs)
+    def push_message(self, *args, **kwargs):
+        print("=== [DEBUG] push_message 被呼叫 ===")
+        print("args:", args)
+        print("kwargs:", kwargs)
+        return super().push_message(*args, **kwargs)
+
+line_bot_api = DebugLineBotApi(os.environ["LINE_CHANNEL_ACCESS_TOKEN"])
 handler = WebhookHandler(os.environ["LINE_CHANNEL_SECRET"])
