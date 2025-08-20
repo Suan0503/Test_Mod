c376300a3c83f47a22b0afa37d7e51c32ad4c233
Suan0503 <a12365478957@gmail.com>
2025-08-20 21:00:53 +0800

    Delete models directory

--- models/blacklist.py
+++
@@ -1,6 +0,0 @@
-from extensions import db
-
-class Blacklist(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), nullable=False)
-    reason = db.Column(db.String(256), nullable=True)
