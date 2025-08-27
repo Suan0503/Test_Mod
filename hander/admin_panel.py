46e72128f72d6c43923a08260ba9ec1435f60555
Suan0503 <a12365478957@gmail.com>
2025-08-27 18:40:47 +0800

    666

--- hander/admin_panel.py
+++ hander/admin_panel.py
@@ -8,8 +8,12 @@ from models import Whitelist, Blacklist, Coupon
 from extensions import db
 
 def init_admin(app):
-    admin = Admin(app, name='後台管理', template_mode='bootstrap3')
+    admin = Admin(app, name='後台管理', template_mode='bootstrap4')
     admin.add_view(ModelView(Whitelist, db.session))
     admin.add_view(ModelView(Blacklist, db.session))
     admin.add_view(ModelView(Coupon, db.session))
+    # 註冊自訂 CSS
+    @app.context_processor
+    def override_admin_css():
+        return dict(admin_css_url='/static/admin_custom.css')
     return admin
