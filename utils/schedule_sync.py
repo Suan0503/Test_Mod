ffbbf7cffb8969b1201195e0b7a97180cab0b282
Suan0503 <a12365478957@gmail.com>
2025-09-04 17:36:39 +0800

    651

--- utils/schedule_sync.py
+++ utils/schedule_sync.py
@@ -1,9 +1,1 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-
-def get_schedule_from_google():
-    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
-    client = gspread.authorize(creds)
-    sheet = client.open('當日 預約表').sheet1  # 請改成你的 Google Sheets 名稱
-    return sheet.get_all_records()
+    # This file has been cleared to remove Google Sheets integration.
