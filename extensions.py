9e67937c78518c7526880d9ccdb632059c18661a
Suan0503 <a12365478957@gmail.com>
2025-09-10 21:41:19 +0800

    612

--- extensions.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from linebot import LineBotApi, WebhookHandler
-import os
-
-db = SQLAlchemy()
-
-# 回復為正式環境，不再覆寫 LineBotApi，取消所有 debug log
-line_bot_api = LineBotApi(os.environ["LINE_CHANNEL_ACCESS_TOKEN"])
-handler = WebhookHandler(os.environ["LINE_CHANNEL_SECRET"])
--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-flask db upgrade
-exec gunicorn app:app -b 0.0.0.0:8080
