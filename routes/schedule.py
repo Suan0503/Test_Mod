1197b39f0b48fcf664af0265e107f7f6e6268017
Suan0503 <a12365478957@gmail.com>
2025-10-04 11:43:38 +0800

    0101

--- routes/schedule.py
+++ routes/schedule.py
@@ -3,9 +3,12 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 
 schedule_bp = Blueprint('schedule', __name__)
 
+
 # 假設時段和妹妹選項暫時硬編碼
-TIME_SLOTS = ['早班', '午班', '晚班']
 GIRLS = ['小美', '小芳', '小玲']
+# 暫存班表資料
+SCHEDULE = []
+
 
 @schedule_bp.route('/schedule', methods=['GET', 'POST'])
 def schedule():
@@ -20,7 +23,13 @@ def schedule():
 			if new_girl not in GIRLS:
 				GIRLS.append(new_girl.strip())
 			girl = new_girl.strip()
-		# 這裡可以加上資料庫儲存或其他邏輯
+		# 新增到班表
+		SCHEDULE.append({
+			'girl': girl,
+			'time': time,
+			'amount': amount,
+			'plan': plan
+		})
 		flash(f'已提交：妹妹={girl}, 時間={time}, 金額={amount}, 方案={plan}', 'success')
 		return redirect(url_for('schedule.schedule'))
-	return render_template('schedule.html', girls=GIRLS)
+	return render_template('schedule.html', girls=GIRLS, schedule=SCHEDULE)
